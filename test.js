4b08a309245d8fcbb1871e004e83dd3e3fe2a630
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import mergeAllRight from "./source.js"
+import mergeAllLeft from "./source"
 
 test(({same, end}) => {
   same(
