9b97b02c7642967e66ecc98d530ae45eb3c7602a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-13 21:17:51 -0700

    Upgrading and using an internal package

--- source.js
+++ source.js
@@ -1,9 +1,10 @@
 import reduceValues from "@unction/reducevalues"
 import mergeRight from "@unction/mergeright"
+import key from "@unction/key"
 import fresh from "@unction/fresh"
 
 const FIRST = 0
 
 export default function mergeAllRight (iterables: Array<IterableType<T>>): IterableType {
-  return reduceValues(mergeRight)(fresh(iterables[FIRST]))(iterables)
+  return reduceValues(mergeRight)(fresh(key(FIRST)(iterables)))(iterables)
 }
