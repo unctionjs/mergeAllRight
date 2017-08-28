3008af2ccce6ba028a59e1e61dcdca83740b8993
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:51:01 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import mergeAllRight from "./source"
+import mergeAllRight from "./"
 
 test(({same, end}) => {
   same(
