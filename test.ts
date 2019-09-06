82ecda84d7de805bfe798bdc880a61fc77749ef9
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:49 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,18 @@
+
+import mergeAllRight from "./index.ts";
+
+test("empty", () => {
+  expect(mergeAllRight([])).toEqual([]);
+});
+
+test("Array", () => {
+  expect(mergeAllRight([["0"], ["1"], ["2"]])).toEqual(["0", "1", "2"]);
+});
+
+test("Object", () => {
+  expect(mergeAllRight([{aaa: "aaa"}, {bbb: "bbb"}, {ccc: "ccc"}])).toEqual({
+    aaa: "aaa",
+    bbb: "bbb",
+    ccc: "ccc",
+  });
+});
